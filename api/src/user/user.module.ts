b06bc40448d632768e50204289666d02015e845f
Mathilde <chambasmat@cy-tech.fr>
2024-05-29 15:11:02 +0200

    changing module for jwt

--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -4,9 +4,22 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UtilsServiceService } from '../utils/utils_service/utils_service.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '60m' },
+      }),
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService, UtilsServiceService],
   exports: [UserService]
