0e99c8444e784b4873dbbecc2e0979920425f51f
eiliel <ameilie@hotmail.fr>
2024-06-14 22:59:31 +0200

    test api url secret

--- front/src/utils/ApiUtils.ts
+++ front/src/utils/ApiUtils.ts
@@ -7,7 +7,7 @@ interface LoginRequestBody {
 }
 
 export default abstract class ApiUtils {
-  private static readonly API_BASE_URL = "https://kollection-blue.vercel.app/";
+  private static readonly API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
   private static readonly API_INSTANCE_JSON = axios.create({
     baseURL: ApiUtils.API_BASE_URL,
     headers: {
