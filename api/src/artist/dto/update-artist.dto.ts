49b39a8a7cb1dc1a39821c6313e3b740953f23c6
marieme <sallmariem@cy-tech.fr>
2024-06-16 12:54:27 +0200

    add image update

--- api/src/artist/dto/update-artist.dto.ts
+++ api/src/artist/dto/update-artist.dto.ts
@@ -1,8 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateArtistDto } from './create-artist.dto';
-import { IsArray } from 'class-validator';
+import { IsArray, IsString } from 'class-validator';
 
 export class UpdateArtistDto {
   @IsArray()
   groups: string[]
+
+  @IsString()
+  image: string
 }
